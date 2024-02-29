9dccff16e0bf0a4e1aafe92027945d68fd4c3f37
Dev <86075203+web-co1@users.noreply.github.com>
2024-03-01 00:46:59 +0500

    Create ne-malibu.js

---
+++ ne-malibu.js
@@ -0,0 +1,322 @@
+/* Moheen */
+var malibu__size_72 = {
+  white: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0fa46840d1af0eb208cdf_kitchen%203%20(1).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0caa43e1d7c6450b11d1d_Malibu%2060%20w%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0cacbeb9044445905ae82_Malibu%2060%20w%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0ca294a08a29d5de0bc22_Malibu%2060%20w%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0caf5af211f65d18364c9_Malibu%2060%20w%203_1%20(1).png",
+    ],
+  },
+  grey: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0f60cc1f7591510f8b2a2_Mask%20group%20(41).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0cc0348782c30d34ed9ef_Malibu%2060%20gray%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0cc289c10b5eb39beeac8_Malibu%2060%20gray%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0cc3ced7647949f056b51_Malibu%2060%20gray%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0cc9cb88593af3617605a_Malibu%2060%20gray%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0ccabb552b79c4ff053ed_Malibu%2060%20gray%203_1%20(1).png",
+    ],
+  },
+
+  dark: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0f9ae718ebf30a9ca5d4d_kitchen%203.jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0cee4c8fefde7393ce6d9_Malibu%2060%20b%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0cefc56e0c07b5a56a32b_Malibu%2060%20b%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0cf109183a254bcb62e2b_Malibu%2060%20b%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0cf258f2be6fc382470a1_Malibu%2060%20b%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0cf37eba105ebe2515222_Malibu%2060%20b%203_1%20(1).png",
+    ],
+  },
+
+  green: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0f6ad6234a5b1ddfd65f9_Mask%20group%20(43).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0cfed5d261c138d7d3f31_Malibu%2060%20g%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0cfff755b2661fafe58ee_Malibu%2060%20g%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d015ed80aa415dba37bb_Malibu%2060%20g%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d02f24a7d63e58e25546_Malibu%2060%20g%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d049efd99600ce687595_Malibu%2060%20g%203_1%20(1).png",
+    ],
+  },
+};
+
+var malibu__size_60 = {
+  white: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0fa46840d1af0eb208cdf_kitchen%203%20(1).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0caa43e1d7c6450b11d1d_Malibu%2060%20w%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0cacbeb9044445905ae82_Malibu%2060%20w%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0ca294a08a29d5de0bc22_Malibu%2060%20w%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0caf5af211f65d18364c9_Malibu%2060%20w%203_1%20(1).png",
+    ],
+  },
+  grey: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0f60cc1f7591510f8b2a2_Mask%20group%20(41).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0cc0348782c30d34ed9ef_Malibu%2060%20gray%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0cc289c10b5eb39beeac8_Malibu%2060%20gray%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0cc3ced7647949f056b51_Malibu%2060%20gray%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0cc9cb88593af3617605a_Malibu%2060%20gray%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0ccabb552b79c4ff053ed_Malibu%2060%20gray%203_1%20(1).png",
+    ],
+  },
+
+  dark: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0f9ae718ebf30a9ca5d4d_kitchen%203.jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0cee4c8fefde7393ce6d9_Malibu%2060%20b%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0cefc56e0c07b5a56a32b_Malibu%2060%20b%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0cf109183a254bcb62e2b_Malibu%2060%20b%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0cf258f2be6fc382470a1_Malibu%2060%20b%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0cf37eba105ebe2515222_Malibu%2060%20b%203_1%20(1).png",
+    ],
+  },
+
+  green: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0f6ad6234a5b1ddfd65f9_Mask%20group%20(43).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0cfed5d261c138d7d3f31_Malibu%2060%20g%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0cfff755b2661fafe58ee_Malibu%2060%20g%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d015ed80aa415dba37bb_Malibu%2060%20g%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d02f24a7d63e58e25546_Malibu%2060%20g%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d049efd99600ce687595_Malibu%2060%20g%203_1%20(1).png",
+    ],
+  },
+};
+
+var malibu__size_48 = {
+  white: {
+    mainImg:
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c0ec09efcfe8e41cd8a78c_kitchen%202%20(14).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d3b4ad1b31fbdfd469b9_Malibu%2048%20w%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d3cb6938254037817d6d_Malibu%2048%20w%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d3f02669a1674852cb68_Malibu%2048%20w%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d4119c10b5eb39c33592_Malibu%2048%20w%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d42c3c7663d3103f815a_Malibu%2048%20w%203%20(1).png",
+    ],
+  },
+  grey: {
+    mainImg:
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c0f03bccc4cb9c461bac56_kitchen%202%20(17).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d4df784f403b8bf62923_Malibu%2048%20gray%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d4ef86681c6deeaf7f8a_Malibu%2048%20gray%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d511b2febad7634ec358_Malibu%2048%20gray%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d52288e23d92315dde10_Malibu%2048%20gray%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d5312c351b6c634868c6_Malibu%2048%20gray%203%20(1).png",
+    ],
+  },
+  dark: {
+    mainImg:
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c0f1757ac9c8597c3190ed_kitchen%202%20(20).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d5d53961df00a681715a_Malibu%2048%20b%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d5e7eba105ebe254d8dc_Malibu%2048%20b%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d5fafc7aa13ed4566604_Malibu%2048%20%20b%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d63e5c4588ffa03f3406_Malibu%2048%20%20b%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d64ee3ef840c6b32c13a_Malibu%2048%20b%203%20(1).png",
+    ],
+  },
+  green: {
+    mainImg:
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c0f2b27e3131f60dc55dca_kitchen%202%20(23).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d6d7a971503212e43702_Malibu%2048%20g%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d6e7dfee1ab15f245342_Malibu%2048%20g%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d6f8d23c576146c4e00d_Malibu%2048%20g%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d70afa2cf7a5c2b3f8bc_Malibu%2048%20g%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d71a4886965751ece5b7_Malibu%2048%20g%203%20(1).png",
+    ],
+  },
+};
+
+var malibu__size_36 = {
+  white: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b86e12074833e8d13e8dcf_kitchen%202%20(12).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d71a4886965751ece5b7_Malibu%2048%20g%203%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d9e64545f1b214a8ddaa_Malibu%2036%20w_2_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0da8183835264cc1cd88e_Malibu%2036%20w_2_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0da93f6d346ed69cf3dc1_Malibu%2036%20w_1_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0daa44f6cca218aeabea1_Malibu%2036%20w%203_1%20(1).png",
+    ],
+  },
+  grey: {
+    mainImg:
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65b87f66d124829fb4db3c61_kitchen%202%20(17).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0db5886ad3a0675dc1272_Malibu%2036%20grey%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0db69bf378f07beb1af52_Malibu%2036%20grey%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0db7b3296f256043b55ff_Malibu%2036%20grey%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0db9ff98b32e407cde075_Malibu%2036%20grey%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0dbb0175eb0446d10a1d6_Malibu%2036%20grey%203_1%20(1).png",
+    ],
+  },
+  dark: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b88c9b303de2d9b064da7c_kitchen%202%20(20).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0dc91d23c576146c7909a_Malibu%2036%20b%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0dca321684e0f4783ac02_Malibu%2036%20b%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0dcb383835264cc1dc324_Malibu%2036%20b%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0dccf682fb1486b961511_Malibu%2036%20b%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0dcf2a24bffce48021dd2_Malibu%2036%20b%203_1%20(1).png",
+    ],
+  },
+  green: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b8e6137ac2740d76da13d6_kitchen%202.png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0dd7f7c6500ea24d097e2_Malibu%2036%20g%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0dd8f548e189f2a2edcb9_Malibu%2036%20g%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0dda0f5739a6b9998ce01_Malibu%2036%20g%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0ddb2e5d2cb6f0464e78b_Malibu%2036%20g%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0ddd6295d1efc66113a97_Malibu%2036%20g%203_1%20(1).png",
+    ],
+  },
+};
+
+var malibu__size_30 = {
+  white: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b86e12074833e8d13e8dcf_kitchen%202%20(12).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d71a4886965751ece5b7_Malibu%2048%20g%203%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0d9e64545f1b214a8ddaa_Malibu%2036%20w_2_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0da8183835264cc1cd88e_Malibu%2036%20w_2_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0da93f6d346ed69cf3dc1_Malibu%2036%20w_1_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0daa44f6cca218aeabea1_Malibu%2036%20w%203_1%20(1).png",
+    ],
+  },
+  grey: {
+    mainImg:
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65b87f66d124829fb4db3c61_kitchen%202%20(17).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0db5886ad3a0675dc1272_Malibu%2036%20grey%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0db69bf378f07beb1af52_Malibu%2036%20grey%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0db7b3296f256043b55ff_Malibu%2036%20grey%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0db9ff98b32e407cde075_Malibu%2036%20grey%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0dbb0175eb0446d10a1d6_Malibu%2036%20grey%203_1%20(1).png",
+    ],
+  },
+  dark: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b88c9b303de2d9b064da7c_kitchen%202%20(20).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0dc91d23c576146c7909a_Malibu%2036%20b%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0dca321684e0f4783ac02_Malibu%2036%20b%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0dcb383835264cc1dc324_Malibu%2036%20b%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0dccf682fb1486b961511_Malibu%2036%20b%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0dcf2a24bffce48021dd2_Malibu%2036%20b%203_1%20(1).png",
+    ],
+  },
+  green: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b8e6137ac2740d76da13d6_kitchen%202.png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0dd7f7c6500ea24d097e2_Malibu%2036%20g%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0dd8f548e189f2a2edcb9_Malibu%2036%20g%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0dda0f5739a6b9998ce01_Malibu%2036%20g%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0ddb2e5d2cb6f0464e78b_Malibu%2036%20g%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0ddd6295d1efc66113a97_Malibu%2036%20g%203_1%20(1).png",
+    ],
+  },
+};
+
+// Get references to size and color buttons
+const sizeButtons01 = document.querySelectorAll("#malibu  .size-btn");
+const colorButtons01 = document.querySelectorAll("#malibu  .variant-btn");
+
+// Function to update images based on selected size and color
+function updateImages01(size, color) {
+  console.log(size, color, "size color");
+  const sizeObject = window[`malibu__size_${size}`];
+
+  const colorObject = sizeObject[color];
+
+  if (sizeObject && colorObject) {
+    // Set main image URL
+    var mainImageElement = document.querySelector("#malibu  .images-block img");
+    if (mainImageElement) {
+      mainImageElement.src = colorObject.mainImg;
+      mainImageElement.srcset = colorObject.mainImg;
+    }
+
+    // Loop through sliders and set new src attributes
+    var sliderImages = document.querySelectorAll("#malibu .slide-item img");
+
+    if (colorObject.sliders.length > 0) {
+      for (var i = 0; i < sliderImages.length; i++) {
+        var index = i % colorObject.sliders.length;
+        var sliderImage = colorObject.sliders[index];
+
+        if (sliderImages[i]) {
+          sliderImages[i].src = sliderImage;
+          sliderImages[i].srcset = sliderImage;
+        }
+      }
+    }
+  }
+}
+
+// Function to handle size button click
+function handleSizeButtonClick01(size) {
+  // Remove 'w--current' class from all size buttons
+  sizeButtons01.forEach((button) => button.classList.remove("size__current"));
+
+  // Add 'w--current' class to the clicked size button
+  this.classList.add("size__current");
+
+ const Color = document.querySelector("#malibu .variant-btn.active")
+    .classList[2];
+
+  // Update images based on the selected size and default color
+  updateImages01(size, Color);
+}
+
+// Function to handle color button click
+function handleColorButtonClick01(color) {
+  // Remove 'active' class from all color buttons
+  colorButtons01.forEach((button) => button.classList.remove("active"));
+
+  // Add 'active' class to the clicked color button
+  this.classList.add("active");
+
+  // Get the selected size button with 'w--current' class
+  const selectedSizeButton = document.querySelector(
+    "#malibu  .size-btn.size__current"
+  );
+
+  // If a size button is selected, update images based on the selected size and color
+  if (selectedSizeButton) {
+    const selectedSize = selectedSizeButton.innerText.trim().split("”")[0]; //output 72"
+    updateImages01(selectedSize, color);
+  }
+}
+
+// Attach click event listeners to size buttons
+sizeButtons01.forEach((button) =>
+  button.addEventListener("click", function () {
+    handleSizeButtonClick01.call(this, this.innerText.trim().split("”")[0]);
+  })
+);
+
+// Attach click event listeners to color buttons
+colorButtons01.forEach((button) =>
+  button.addEventListener("click", function () {
+    handleColorButtonClick01.call(this, this.classList[2]); // Assuming the color class is the second class in the classList
+  })
+);
